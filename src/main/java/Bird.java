1c81f914ae516001dc685ed27c50a9d50b948069
teriannaj28 <teriannaj@gmail.com>
2023-02-20 23:31:24 +0000

    Coding Lab: Intro to OOP (Week 2: Day 1)

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -45,7 +45,15 @@ public class Bird {
      *         greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        String sound;
+        
+        if (this.ageMonths < 3) {
+            sound = "Tweet";
+        } else {
+            sound = "Chirp";
+        }
+        
+        return sound;
     }
 
 }
